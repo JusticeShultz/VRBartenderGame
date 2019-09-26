078d3da8a96391d719837b3625ead0a10ec78753
Ray Soyama <raysoyama@gmail.com>
2019-09-25 18:03:08 -0700

    Added two scripts

---
+++ Assets/_Bartender VR/Scripts/DrinkSystem/DrinkSystemManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkSystemManager : MonoBehaviour
+{
+    public enum DrinkIngredients
+    {
+        Ice,
+        Ingredient1,
+        Ingredient2,
+        Ingredient3,
+        Ingredient4,
+        Ingredient5,
+    }
+    
+
+
+    /*
+     * 3 Portions
+     * Each 'pump' is a portion
+     * adding to cup layers in colors
+     */
+
+    void Update()
+    {
+        
+    }
+}
