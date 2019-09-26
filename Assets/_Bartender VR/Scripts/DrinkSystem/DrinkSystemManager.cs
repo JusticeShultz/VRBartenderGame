b61699fd8c367d7e97d38caa2867e8a44abeac00
Ray Soyama <raysoyama@gmail.com>
2019-09-26 11:24:19 -0700

    Base Data structure for the drink system

--- Assets/_Bartender VR/Scripts/DrinkSystem/DrinkSystemManager.cs
+++ Assets/_Bartender VR/Scripts/DrinkSystem/DrinkSystemManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DrinkSystemManager : MonoBehaviour
 {
@@ -13,8 +15,38 @@ public class DrinkSystemManager : MonoBehaviour
         Ingredient4,
         Ingredient5,
     }
-    
 
+    public enum DrinkNames
+    {
+        DevDrink1,
+        DevDrink2,
+        DevDrink3,
+        DevDrink4,
+        DevDrink5
+    }
+
+    //Dictionary Aids (TLDR. Unity dont have dictionary support in Inspector, So we make it in arrays, then make the dictionary at start)
+    [SerializeField] [ReadOnlyField] //This needs to be manually updated when we add and subtract drinks
+    private List<DrinkNames> drinkListNames = new List<DrinkNames> { DrinkNames.DevDrink1,
+                                                                     DrinkNames.DevDrink2,
+                                                                     DrinkNames.DevDrink3,
+                                                                     DrinkNames.DevDrink4,
+                                                                     DrinkNames.DevDrink5};
+
+    [SerializeField]
+    private List<IngredientSO> drinkListIngredients;
+
+    private Dictionary<DrinkNames, IngredientSO> drinkList = new Dictionary<DrinkNames, IngredientSO>();
+
+    [HideInInspector]
+    public Dictionary<DrinkNames, IngredientSO> DrinkList
+    {
+        get
+        {
+            return drinkList;
+        }
+    }
+    //Dictionary Aids
 
     /*
      * 3 Portions
@@ -22,8 +54,30 @@ public class DrinkSystemManager : MonoBehaviour
      * adding to cup layers in colors
      */
 
-    void Update()
+
+    void Start()
     {
-        
+        //Initialzed the Dictionary
+        for(int i = 0; i < drinkListNames.Count; i++)
+        {
+            drinkList.Add(drinkListNames[i], drinkListIngredients[i]);
+        }
     }
+
+    //Returns a Random Drink
+    public DrinkNames RequestRandomDrink()
+    {
+        return drinkListNames[Random.Range(0, drinkList.Count)];
+    }
+
+    /*
+     * TODO 
+     * Create a Drink 
+     * Drink validation Check (when served) (take in an enum)
+     * Drink Clear
+     * 
+     */
+
+
+
 }
---
+++ Assets/_Bartender VR/Scripts/DrinkSystem/DrinkSystemUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkSystemUnitTest : MonoBehaviour
+{
+    DrinkSystemManager drinkGod;
+
+    public List<DrinkSystemManager.DrinkNames> RandomDrinks;
+
+    void Start()
+    {
+        drinkGod = GetComponent<DrinkSystemManager>();
+
+        //GetRandomDrinksTest(10);
+    }
+
+    void GetRandomDrinksTest(int count)
+    {
+        RandomDrinks.Clear();
+
+        for (int i = 0; i < count; i++)
+        {
+            RandomDrinks.Add(drinkGod.RequestRandomDrink());
+        }
+    }
+
+
+
+
+}
