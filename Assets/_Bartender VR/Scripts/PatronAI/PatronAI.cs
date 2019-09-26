7d78e2455f07972436d9f1c719c7c7e0cb8bce34
Carter <carter.patterson95@gmail.com>
2019-09-26 12:55:26 -0700

    make my scene, start on ai script elements

---
+++ Assets/_Bartender VR/Scripts/PatronAI/PatronAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatronAI : MonoBehaviour
+{
+
+    public DrinkSystemManager.DrinkNames desiredDrink;
+
+    public GameObject desiredLocation { get; set; }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+}
