65c6214bd346f0171e812f971666c173d9e02f45
Justice Shultz <s189081@students.aie.edu>
2019-10-02 17:12:00 -0700

    Uh

---
+++ Assets/_Bartender VR/Scripts/DrinkSystem/DrinkFillSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkFillSystem : MonoBehaviour
+{
+    public static bool ShakerInHand = false;
+
+    public float FillTime = 2.0f;
+    public GameObject Shaker;
+    public GameObject Cup;
+
+    public Valve.VR.InteractionSystem.VelocityEstimator Ve;
+    public Valve.VR.InteractionSystem.Interactable Intble;
+    public Valve.VR.InteractionSystem.Throwable Thrbl;
+    public Valve.VR.InteractionSystem.InteractableHoverEvents IHE;
+    public Valve.VR.SteamVR_Skeleton_Poser Psr;
+
+    void Start()
+    {
+        ShakerInHand = false;
+    }
+
+    void Update()
+    {
+        Cup.SetActive(ShakerInHand);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.name);
+        StartCoroutine(DoFill());
+        Ve.enabled = false;
+        Intble.enabled = false;
+        Thrbl.enabled = false;
+        IHE.enabled = false;
+        Psr.enabled = false;
+    }
+
+    public void PickUpShaker()
+    {
+        ShakerInHand = true;
+    }
+
+    public void DropShaker()
+    {
+        ShakerInHand = false;
+    }
+
+    IEnumerator DoFill()
+    {
+        yield return new WaitForSeconds(FillTime);
+    }
+}
