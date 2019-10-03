1226ce9a49d5dc8c88040cd892b3f6dc0bc38f73
Justice Shultz <s189081@students.aie.edu>
2019-10-03 15:11:34 -0700

    Merg

---
+++ Assets/_Bartender VR/Scripts/PlayerController/ShakeTracker.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class ShakeTracker : MonoBehaviour
+{
+    [System.Serializable] public class ShakeEvent : UnityEvent { }
+
+    public float Sensitivity = 0.1f;
+    public float ShakeTime = 3f;
+    public ShakeEvent onShake;
+    public ShakeEvent stopShake;
+    public ShakeEvent finishedShaking;
+    
+    private Vector3 lastPosition;
+    private float _ShakeTime = 0f;
+
+    void Update()
+    {
+        Vector3 difference = transform.position - lastPosition;
+
+        if (difference.magnitude >= Sensitivity)
+        {
+            _ShakeTime += Time.deltaTime;
+            onShake.Invoke();
+
+            if (_ShakeTime >= ShakeTime)
+            {
+                _ShakeTime = 0f;
+                finishedShaking.Invoke();
+            }
+        }
+        else
+        {
+            if (_ShakeTime > 0f)
+                stopShake.Invoke();
+
+            _ShakeTime = 0f;
+        }
+
+        lastPosition = transform.position;
+    }
+}
