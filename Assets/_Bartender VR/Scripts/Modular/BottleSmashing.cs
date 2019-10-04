176303929c6f8d8e414072602a9cca5a779939d9
JusticeShultz <shultzjc1@gmail.com>
2019-10-04 14:47:33 -0700

    You can now shatter beer bottles to make a shattered beer bottle weapon

---
+++ Assets/_Bartender VR/Scripts/Modular/BottleSmashing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleSmashing : MonoBehaviour
+{
+    public float DestroySensitivity = 10f;
+    public GameObject DestroyedState;
+    public Valve.VR.InteractionSystem.VelocityEstimator estimator;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (estimator.GetAccelerationEstimate().magnitude >= DestroySensitivity)
+        {
+            Instantiate(DestroyedState, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
