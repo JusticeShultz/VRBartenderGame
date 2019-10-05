95a38e9a0addcd3781462c80897e28dc050cfd2f
Ray Soyama <raysoyama@gmail.com>
2019-10-04 17:34:11 -0700

    Hand Menu System working, need hooking up to

---
+++ Assets/_Bartender VR/Scripts/DrinkSystem/MenuShader/HandMenu.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMenu : MonoBehaviour
+{
+    //HandMenu System
+    [SerializeField]
+    private DrinkSystemManager DrinkManager;
+
+
+    [Space(10)]
+    [Header("Material Shit")]
+
+    [SerializeField]
+    private MeshRenderer MenuDrinkRenderer;
+
+    [SerializeField]
+    private List<Material> ServingCountMat;
+
+    [SerializeField]
+    private List<MeshRenderer> ServingMesh;
+
+    private Dictionary<DrinkSystemManager.DrinkIngredients, MeshRenderer> MenuIngredientsMesh = new Dictionary<DrinkSystemManager.DrinkIngredients, MeshRenderer>();
+
+    void OnEnable()
+    {
+        if (MenuIngredientsMesh.Count == 0)
+        {
+            return;
+        }
+
+        SetHandMenuMaterial();
+    }
+
+
+    void Start()
+    {
+        for (int i = 0; i < ServingMesh.Count; i++)
+        {
+            MenuIngredientsMesh.Add((DrinkSystemManager.DrinkIngredients)i, ServingMesh[i]);
+        }
+
+
+        SetHandMenuMaterial();
+    }
+
+
+    public void SetHandMenuMaterial()
+    {
+        //Change the Menu Drink Icon
+        //MenuDrinkRenderer.materials[0].SetTexture("_MainTex", DrinkManager.DrinkList.[DrinkOnMenu].DrinkImg);
+        if (DrinkManager.MyDrink.Count >= 5)
+        {
+            MenuDrinkRenderer.gameObject.SetActive(true);
+            MenuDrinkRenderer.material.SetTexture("_MainTex", DrinkManager.DrinkList[DrinkManager.ScanDrink()].DrinkImg);
+        }
+        else
+        {
+            MenuDrinkRenderer.gameObject.SetActive(false);
+        }
+        //Add Shake shit
+
+        //Change the ingredient serving
+        foreach (DrinkSystemManager.DrinkIngredients ingredient in Enum.GetValues(typeof(DrinkSystemManager.DrinkIngredients)))
+        {
+
+            //For every ingredient, check how many are in the recipie, then check how many in your drink. If they don't match, they aint the same
+            int ingredientsInDrink = 0;
+
+            foreach (DrinkSystemManager.DrinkIngredients ing in DrinkManager.MyDrink)
+            {
+                if (ing == ingredient)
+                {
+                    ingredientsInDrink++;
+                }
+            }
+
+
+            try
+            {
+                MenuIngredientsMesh[ingredient].material = ServingCountMat[ingredientsInDrink];
+            }
+            catch (KeyNotFoundException ass)
+            {
+                Debug.Log($"{ingredient} and {ingredientsInDrink}");
+                throw;
+            }
+        }
+    }
+}
